90e77adad15ca9b9646970a089fdcc9794ccb44f
Sefton Fe <fesefton2000@gmail.com>
2020-03-31 23:00:18 +0800

    文件类练习

--- src/practice/day4/Traversal_Cdisk.java
+++ src/practice/day4/Traversal_Cdisk.java
@@ -8,17 +8,21 @@ public class Traversal_Cdisk {
         System.out.println("请输入遍历文件路径");
         traversal(new Scanner(System.in).nextLine());
     }
-    public static void traversal(String path){
+    private static void traversal(String path) {
         File file = new File(path);
-        File[] files = file.listFiles();
-        File maxF,minF;
-        maxF = files[0];minF = files[0];
-        for (int i = 1; i < files.length; i++) {
-            if(maxF.length() < files[i].length())
-                maxF = files[i];
-            if(minF.length() > files[i].length())
-                minF = files[i];
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            File maxF, minF;
+            maxF = files[0];
+            minF = files[0];
+            for (int i = 1; i < files.length; i++) {
+                if (maxF.length() < files[i].length())
+                    maxF = files[i];
+                if (minF.length() > files[i].length())
+                    minF = files[i];
+            }
+            System.out.println("最小文件是：" + minF.getName() + "  最大文件是：" + maxF.getName());
         }
-        System.out.println("最小文件是：" + minF.getName() + "  最大文件是：" + maxF.getName());
+        else System.out.println("文件路径不存在");
     }
 }
